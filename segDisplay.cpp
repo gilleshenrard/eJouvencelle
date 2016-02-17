28effb76d5e3bf460b85ea4a7a7a4baaf09517f8
Gilles Henrard <gilles.henrard@hotmail.com>
2016-02-17 21:54:35 +0100

    Replaced code by modifications

--- segDisplay.cpp
+++ segDisplay.cpp
@@ -1,49 +1,91 @@
 #include "segDisplay.h"
 
+/************************************************************/
+/*  I : /                                                   */
+/*  P : Builds and initialises a new 7-segment display      */
+/*  O : /                                                   */
+/************************************************************/
 segDisplay::segDisplay()
-{}
+{
+  pinSet=false;
+  dot=0;
 
+  numbers[0].display=126;
+  numbers[1].display=48;
+  numbers[2].display=109;
+  numbers[3].display=121;
+  numbers[4].display=51;
+  numbers[5].display=91;
+  numbers[6].display=95;
+  numbers[7].display=112;
+  numbers[8].display=127;
+  numbers[9].display=123;
+}
+
+/************************************************************/
+/*  I : /                                                   */
+/*  P :                                                     */
+/*  O : /                                                   */
+/************************************************************/
 segDisplay::~segDisplay()
 {}
 
-void segDisplay::setSeg(int a, int b, int c, int d, int e, int f, int g, int newdot)
-{
-  segments[0]=a;
-  segments[1]=b;
-  segments[2]=c;
-  segments[3]=d;
-  segments[4]=e;
-  segments[5]=f;
-  segments[6]=g;
-
-  dot=newdot;
-}
-
+/************************************************************/
+/*  I : /                                                   */
+/*  P :                                                     */
+/*  O : /                                                   */
+/************************************************************/
 void segDisplay::setupPins()
 {
   for(int i=0 ; i<7 ; i++)
-    pinMode(segments[i], OUTPUT);
-
-  pinMode(dot, OUTPUT);
+    pinMode(pins[i], OUTPUT);
 }
 
+/************************************************************/
+/*  I : Number to display                                   */
+/*  P : Sets the pin values of the display to display the   */
+/*    received number                                       */
+/*  O : /                                                   */
+/************************************************************/
 void segDisplay::displayNumber(int Number)
 {
-  for(int i=0 ; i<7 ; i++)
-    digitalWrite(segments[i], numbers[Number][i]);
+  /* Tests, bit by bit, the values defined for each pin into numbers[num].
+    Starting from 64 to 0, the test will define if the current bit is true or false,
+    then shift to the right*/
+
+  for(unsigned char i=64, j=0 ; i>0 ; i/=2, j++)
+    if(numbers[num]&i)
+      digitalWrite(segments[j], HIGH);    //if test value != 0 -> true then HIGH
+    else
+      digitalWrite(segments[j], LOW);     //else, the bit is set to 0, thus the signal is LOW
 }
 
+/************************************************************/
+/*  I : /                                                   */
+/*  P : Turns the display off                               */
+/*  O : /                                                   */
+/************************************************************/
 void segDisplay::noDisplay()
 {
-  for(int i=0 ; i<7 ; i++)
+  for(i=0 ; i<7 ; i++)
     digitalWrite(segments[i], LOW);
 }
 
+/************************************************************/
+/*  I : /                                                   */
+/*  P : Turns the dot on the display on                     */
+/*  O : /                                                   */
+/************************************************************/
 void segDisplay::displayDot()
 {
     digitalWrite(dot, HIGH);
 }
 
+/************************************************************/
+/*  I : /                                                   */
+/*  P : Turns the dot on the display off                    */
+/*  O : /                                                   */
+/************************************************************/
 void segDisplay::noDot()
 {
     digitalWrite(dot, LOW);
--- segDisplay.h
+++ segDisplay.h
@@ -2,34 +2,34 @@
 #define SEGDISPLAY_H_INCLUDED
 #include<Arduino.h>
 
+typedef union
+{
+	unsigned char display;
+	typedef struct
+	{
+		int dot:1;
+		int a:1;
+		int b:1;
+		int c:1;
+		int d:1;
+		int e:1;
+		int f:1;
+		int g:1;
+	}segments;
+}t_display;
+
 class segDisplay
 {
 	private:
-		union segments
-		{
-			int all[7]={0};
-
-			typedef struct
-			{
-				int a=0;
-				int b=0;
-				int c=0;
-				int d=0;
-				int e=0;
-				int f=0;
-				int g=0;
-			}specific;
-		};
-		int numbers[3][7]={{HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW},
-                    	   {LOW, LOW, HIGH, HIGH, LOW, LOW, LOW},
-                    	   {HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH}};
-		int dot=0;
+		bool pinSet;
+		int dot;
+		int pins[7];
+		t_display numbers[10];
 
 	public:
 		segDisplay();
 		~segDisplay();
 
-		void setSeg(int, int, int, int, int, int, int, int);
 		void setupPins();
 
 		void displayNumber(int);

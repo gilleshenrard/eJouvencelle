470f3b748c742a2a5cd8f1788107dd68b5fa4866
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-02-18 10:57:23 +0100

    Put numbers[] as static and directly initialised

--- segDisplay.h
+++ segDisplay.h
@@ -20,11 +20,21 @@ typedef union
 
 class segDisplay
 {
+	static const t_display numbers[10]={{.display=126},
+						{.display=48},
+						{.display=109},
+						{.display=121},
+						{.display=51},
+						{.display=91},
+						{.display=95},
+						{.display=112},
+						{.display=127},
+						{.display=123}};
+
 	private:
 		bool pinSet;
 		int dot;
 		int pins[7];
-		t_display numbers[10];
 
 	public:
 		segDisplay();

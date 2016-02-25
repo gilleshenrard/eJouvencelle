b76092250395f5c1dfb5205c21908105d329b8ee
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-02-25 09:35:33 +0100

    added NotesLength and associated setter
    meant to manage notes length (in portions of BPM)

--- Music.h
+++ Music.h
@@ -6,14 +6,15 @@ class Music{
   private:
       int pin;
       int *Notes;
-      int notesSize;
+      int *NotesLength;
       int noteCount;
 
   public:
       Music(int);
-      Music(int, int[], const int);
+      Music(int, int[], int[], const int);
       ~Music();
       void setNotes(int[], const int);
+      void setNotesLength(int[], const int);
       void setup();
       void start();
       void stop();

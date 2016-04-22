3700cfd1280da745e6983f360b67e5ee31bf9616
Gilles Henrard <gilles.henrard@hotmail.com>
2016-04-23 01:14:39 +0200

    Testing and buttons
    - added 2 buttons (start and reset)
    - changed values in order to test
    - attached interruptions
    
    TO REVIEW

--- eJouvencelle.ino
+++ eJouvencelle.ino
@@ -1,36 +1,35 @@
 #include"segDisplay.h"
 #include"Music.h"
 
+int startbutton=3;
+int resetbutton=2;
+
 unsigned long prevTime = millis();
 bool started=false;
 
 int notes[5]={440, 550, 660, 770, 880};
-int length[5]={2, 4, 4, 2, 0};
-Music melody = Music(3, notes, 5, length, 60);
+int length[5]={2, 4, 4, 2, 2};
+Music melody = Music(12, notes, 5, length, 60);
 
-segDisplay display = segDisplay(2, 3, 5, 6, 7, 8, 10);
-int number = 0, dot=LOW;
+segDisplay display = segDisplay(7, 8, 11, 10, 9, 6, 5);
+int number = 0;
 
 void setup() {
   display.setup();
   display.display(number);
-  display.setDot(11);
   melody.setup();
+
+  pinMode(startbutton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(startbutton), startbuttonfell, FALLING);
+  pinMode(resetbutton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(resetbutton), resetbuttonfell, FALLING);
 }
 
 void loop() {
   unsigned long newTime = millis();
-
-  if(!started)
-  {
-    melody.start();
-    started=true;
-  }
   
-  if(newTime-prevTime >= 1000)
+  if(newTime-prevTime >= 1000 && started)
   {
-    dot = !dot;
-    display.displayDot(dot);
     number++;
     number%=10;
     display.display(number);
@@ -38,4 +37,18 @@ void loop() {
   }
 
   melody.refresh(newTime);
+}
+
+void startbuttonfell()
+{
+  started=!started;
+  if(started)
+    melody.start();
+  else
+    melody.stop();
+}
+
+void resetbuttonfell()
+{
+  melody.reset();
 }

1c5651e9705b4b3ad2ca0c01f0f2670c7eaca9c7
Gilles Henrard <gilles.henrard@hotmail.com>
2019-08-20 20:44:06 +0200

    make tests more straightforward + add pinouts in comments

--- test_CD4511/test_CD4511.ino
+++ test_CD4511/test_CD4511.ino
@@ -1,22 +1,48 @@
 #include "CD4511.h"
 
+/*
+ * CD4511 library testing procedure
+ * --------------------------------
+ *      CD4511 pins   |   Arduino pins
+ *      B : 1         |   6 
+ *      C : 2         |   7
+ *      Test : 3      |   +5V
+ *      Blank : 4     |   10
+ *      Latch : 5     |   9
+ *      D : 5         |   8
+ *      A : 7         |   5
+ *      VSS : 8       |   GND
+ *      VDD : 16      |   +5V
+ *      
+ *      CD4511 pins   |   7 segment display (pin 1 bottom left)
+ *      e : 9         |   1
+ *      d : 10        |   2
+ *      c : 11        |   4
+ *      b : 12        |   6
+ *      a : 13        |   7
+ *      g : 14        |   10
+ *      f : 15        |   9
+ *                    |   3 : GND
+ *                    |   8 : GND
+ */
+
 CD4511 displ = CD4511(5, 6, 7, 8, 9, 10);
-String msg = "", tmp = "";
-int nb = 0, blk = 0;
 
 void setup() {
-  Serial.begin(9600);
   displ.setup();
 }
 
 void loop() {
-  int tmp = LOW;
-  digitalWrite(LED_BUILTIN, HIGH);
-  
+  //turn on the display
   displ.displayON();
 
+  //display all numbers from 0 to 9 (for 0.5s each)
   for(int i=0 ; i<10 ; i++){
-    displ.display(i);
+    displ.setNumber(i);
     delay(500);
   }
+
+  //turn off the screen for 1s
+  displ.displayOFF();
+  delay(1000);
 }

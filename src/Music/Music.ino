9d19a8415237aca3772ad1ad42984cf24b0f9ae7
Gilles Henrard <gilles.henrard@hotmail.com>
2020-01-29 11:58:36 +0100

    copy the music code from the final project

--- src/Music/Music.ino
+++ src/Music/Music.ino
@@ -1,12 +1,23 @@
 #import "Music.h"
 #import "notes.h"
 
+int notes[28]={NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5,
+               NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5,
+               NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_E5 /*suspension*/, NOTE_D5,
+               NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5};
+int length[28]={2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2};
+
+int BeatsPM = 100;
+Music melody = Music(11, notes, 28, length, BeatsPM);
+
 void setup() {
-  // put your setup code here, to run once:
+  melody.setup();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long newTime = millis();
 
+  melody.start(newTime);
+  melody.refresh(newTime);
 }

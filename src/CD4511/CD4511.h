03fa85e7eef4b2d8ad00660dcb41608af13849fc
Gilles Henrard <gilles.henrard@hotmail.com>
2020-02-06 11:37:32 +0100

    merge bit field and union

--- src/CD4511/CD4511.h
+++ src/CD4511/CD4511.h
@@ -2,19 +2,17 @@
 #define CD4511_H_INCLUDED
 #include<Arduino.h>
 
-typedef struct
-{
-  char a:1;
-  char b:1;
-  char c:1;
-  char d:1;
-  char buf:4;
-}output_t;
-
 typedef union
 {
   char nb;
-  output_t pinsval;
+  struct
+  {
+    char a:1;
+    char b:1;
+    char c:1;
+    char d:1;
+    char buf:4;
+  }pinsval;
 }output_u;
 
 class CD4511
@@ -29,7 +27,7 @@ class CD4511
 		void setup();
 
 		void setNumber(int num);
-    		void commit();
+		void commit();
 		void displayOFF();
 		void displayON();
 };

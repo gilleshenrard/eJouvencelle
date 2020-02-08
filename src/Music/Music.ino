6f61501961d9955640a3078ad38d6d6f4177a0d1
Gilles Henrard <gilles.henrard@hotmail.com>
2020-02-08 16:09:10 +0100

    rearrange comments

--- src/Music/Music.ino
+++ src/Music/Music.ino
@@ -1,7 +1,7 @@
 #import "Music.h"
 #import "notes.h"
 
-//timer values (0x3D08 = 1Hz)
+//timer1 values (0x3D08 = 1Hz because (16*10^6) / (1*1024) - 1)
 #define BPM120 0xF42 // 4Hz tick (8th note at 120 BPM)
 
 int notes[28]={NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5,
@@ -14,31 +14,47 @@ volatile bool toggle1 = true;
 int BeatsPM = 120;
 Music melody = Music(11, notes, 28, length, BeatsPM);
 
+
+/****************************************************************************/
+/*  I : /                                                                   */
+/*  P : Setup procedure                                                     */
+/*  O : /                                                                   */
+/****************************************************************************/
 void setup() {
+  //stop interrupts
+  cli();
+  
+  //setup melody and pin 13 (test led)
   melody.setup();
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
 
-  cli();//stop interrupts
+  //set timer1 interrupt at 4Hz
+  TCCR1A = 0;
+  TCCR1B = 0;
 
-  //set timer1 interrupt at 1Hz
-  TCCR1A = 0;// set entire TCCR1A register to 0
-  TCCR1B = 0;// same for TCCR1B
-  TCNT1  = 0;//initialize counter value to 0
-  // set compare match register for 1hz increments
+  //reset counter value +
+  //set compare match register for 4hz increments
+  TCNT1  = 0;
   OCR1A = BPM120;
+
   // turn on CTC mode
+  // + Set prescaler to 1024
+  // + enable timer compare interrupt
   TCCR1B |= (1 << WGM12);
-  // Set CS10 and CS12 bits for 1024 prescaler
   TCCR1B |= (1 << CS12) | (1 << CS10);  
-  // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
-  sei();//allow interrupts
+  //allow interrupts
+  sei();
 }
 
-ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
-//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+/****************************************************************************/
+/*  I : timer1 comparator vector                                            */
+/*  P : toggle pin 13 at 4Hz (2Hz full wave)                                */
+/*  O : /                                                                   */
+/****************************************************************************/
+ISR(TIMER1_COMPA_vect){
   if (toggle1){
     digitalWrite(13,HIGH);
     toggle1 = 0;
@@ -49,6 +65,11 @@ ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
   }
 }
 
+/****************************************************************************/
+/*  I : /                                                                   */
+/*  P : Main loop                                                           */
+/*  O : /                                                                   */
+/****************************************************************************/
 void loop() {
   unsigned long newTime = millis();
 

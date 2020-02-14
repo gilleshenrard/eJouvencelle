e3b633ac190dea41e4725f12b52eed2cef92334e
Gilles Henrard <gilles.henrard@hotmail.com>
2020-02-14 16:21:26 +0100

    implement time-based MML method (compiled with g++, to modify)

--- src/Music/Music.cpp
+++ src/Music/Music.cpp
@@ -55,6 +55,113 @@ void Music::start(unsigned long curTime){
   }
 }
 
+/****************************************************************/
+/*  I : Music MML string to decode                              */
+/*  P : When a tick is reached, decode a note and play it       */
+/*  O : NOTEERR : error while decoding                          */
+/*      NOTEOK  : note properly decoded                         */
+/*      LASTNOTE: no further notes afterwards                   */
+/*      ENDNOTE : last note has been played, end of music       */
+/****************************************************************/
+int Music::onTick(std::string& music)
+{
+    //check if note duration has been reached
+    if(this->m_nbtick > 0)
+    {
+        this->m_nbtick--;
+        return NOTEOK;
+    }
+
+    //NOTE UPDATE
+
+    //was the last tick the last note?
+    if(this->m_nextNote == std::string::npos)
+        return ENDNOTE;
+
+    //roll the music to the right (nextNote is off by 1 increment)
+    this->m_curNote = this->m_nextNote;
+    this->m_nextNote = music.find_first_of(' ', this->m_curNote+1);
+
+    //are there any notes left to decode?
+    if(this->m_nextNote == std::string::npos)
+        return LASTNOTE;
+
+    //update nextNote to point to an actual note
+    this->m_nextNote ++;
+
+
+    //NOTE DECODING
+
+    //get the code for the current note
+    std::string note = music.substr(this->m_curNote, this->m_nextNote);
+    std::string::iterator it = note.begin();
+
+    //decode eventual octave change
+    if(isdigit(*it))
+    {
+        this->m_octave = *it - 48; //translate from ASCII -> shift 48
+        it++;
+    }
+
+    //decode the note requested (7th octave by default)
+    float frequency = 0;
+    switch(*it)
+    {
+        case 'a':
+            frequency = 2093.0;
+            break;
+        case 'b':
+            frequency = 2349.0;
+            break;
+        case 'c':
+            frequency = 2637.0;
+            break;
+        case 'd':
+            frequency = 2794.0;
+            break;
+        case 'e':
+            frequency = 3136.0;
+            break;
+        case 'f':
+            frequency = 3520.0;
+            break;
+        case 'g':
+            frequency = 3951.0;
+            break;
+        case 'r':
+            frequency = 0.0;
+            break;
+    }
+
+    //shift the note to the right octave
+    frequency = frequency / pow(2.0, (7.0 - (float)this->m_octave));
+    it++;
+
+    //decode sharp or flat notes
+    if ((*it == '#') || (*it =='+'))
+    {
+        frequency *= 1.059;
+        it++;
+    }
+    if (*it == '-')
+    {
+        frequency /= 1.059;
+        it++;
+    }
+
+    //decode note duration (nb of ticks = nb of 1/32 notes)
+    int index = note.find(*it);
+    note = note.substr(index);
+    unsigned char duration = (char)stoi(note);
+    this->m_nbtick = 32 / duration;
+
+    //decode dotted note (duration * 1.5)
+    if (note.find(".") != std::string::npos)
+        this->m_nbtick = (unsigned char)((float)this->m_nbtick * 1.5);
+
+    return NOTEOK;
+}
+
 /****************************************************************
  * I : /                                                        *
  * P : Stops playing                                            *
--- src/Music/Music.h
+++ src/Music/Music.h
@@ -1,6 +1,14 @@
 #ifndef MUSIC_H_INCLUDED
 #define MUSIC_H_INCLUDED
 #include <Arduino.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+#define NOTEERR -1
+#define NOTEOK 0
+#define LASTNOTE 1
+#define ENDNOTE 2
 
 class Music{
   private:
@@ -14,6 +22,10 @@ class Music{
       bool isFinished;
       bool lastnote;
       bool isStarted;
+      size_t          m_curNote;
+      size_t          m_nextNote;
+      unsigned char   m_octave;
+      unsigned char   m_nbtick;
 
   public:
       Music(int Pin, int notes[]=NULL, const int notesSz=0, int notesLen[]=NULL, int newBPM=0);
@@ -21,6 +33,7 @@ class Music{
       void setBPM(int);
       void setup();
       void start(unsigned long curTime);
+      int onTick(std::string& music);
       void stop();
       void reset();
       void refresh(unsigned long curTime);
